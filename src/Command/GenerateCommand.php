81c1d1f7ff7a6a372476ec838f3ceed372015c1b
vntw <codesguard@cersei.de>
2024-08-19 00:16:32 +0200

    Make stuff work again and deploy

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -20,19 +20,21 @@ class GenerateCommand extends Command
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
         $outDir = $input->getArgument('outdir');
 
         if (!is_dir($outDir)) {
-            (new Filesystem())->mkdir($outDir, 0644);
+            (new Filesystem())->mkdir($outDir, 0755);
         }
 
         $generator = new LocalStaticGenerator($outDir);
         $generator->generate()->write();
 
         $io->success('Written to: '.realpath($outDir));
+
+        return Command::SUCCESS;
     }
 }
